b3af7a4d6d93c80d448bf68b51336570924eba56
jijinkh <jijinkharidas10@gmail.com>
2019-08-31 18:26:16 +0530

    new template added

--- Sporx-project/organization/urls.py
+++ Sporx-project/organization/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login,name='login'),
+    path('login', views.login, name='login'),
+    path('login_success', views.login_succes, name='login_success')
 ]
--- Sporx-project/organization/views.py
+++ Sporx-project/organization/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'organization/login.html')
+
+def login_succes(request):
+    return render(request, 'organization/login.html')
